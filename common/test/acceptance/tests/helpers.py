3c498b3d25878564780d32967d00312b62516e71
Ben Patterson <bpatterson@edx.org>
2015-02-28 13:43:31 -0500

    Use mixin for cohort event-related tests.

--- common/test/acceptance/tests/helpers.py
+++ common/test/acceptance/tests/helpers.py
@@ -248,13 +248,14 @@ def get_modal_alert(browser):
 
 
 class EventsTestMixin(object):
-
+    """
+    Helpers and setup for running tests that evaluate events emitted
+    """
     def setUp(self):
         super(EventsTestMixin, self).setUp()
         self.event_collection = MongoClient()["test"]["events"]
         self.start_time = datetime.now()
         self.addCleanup(self.cleanup)
-
 
     def assert_event_emitted_num_times(self, event_name, event_time, event_user_id, num_times_emitted):
         """
@@ -275,6 +276,9 @@ class EventsTestMixin(object):
         )
 
     def cleanup(self):
+        """
+        Remove the events from mongo
+        """
         self.event_collection.drop()
 
 
@@ -298,8 +302,6 @@ class UniqueCourseTest(WebAppTest):
             'run': 'test_run',
             'display_name': 'Test Course' + self.unique_id
         }
-
-
 
     @property
     def course_id(self):
